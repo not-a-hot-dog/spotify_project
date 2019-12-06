a3897fa4a319ad668106d39c65f85dd4aaf6f550
mattfinney <MattFinney@users.noreply.github.com>
2019-12-05 19:44:50 -0500

    Splitting the clustering and pre-processing. The clustering is quick. The pre-processing takes time.

--- model/k_clustering_pre-processing.py
+++ model/k_clustering_pre-processing.py
@@ -5,9 +5,9 @@ from sklearn.cluster import KMeans
 import pickle
 
 print('Finding playlist features')
-playlist_list = np.genfromtxt('./data/train_pids.csv', skip_header=1, dtype=int)
+playlist_list = np.genfromtxt('./data/train_pids.csv', skip_header=0, dtype=int)
 playlist_features = build_playlist_features(playlist_list)
-playlist_features.to_csv('./data/playlist_features.csv')
+playlist_features.to_csv('./data/playlist_features_train.csv')
 
 print('Finding top artists')
 # Find the top artists who dominate playlists
@@ -22,28 +22,7 @@ playlist_features.artist_uri_top = playlist_features.artist_uri_top[artists_to_k
 playlist_features.artist_uri_freq = playlist_features.artist_uri_freq[artists_to_keep]
 playlist_features.artist_uri_freq.fillna(0, inplace=True)
 
-
 top_artist_dummies = pd.get_dummies(playlist_features.artist_uri_top)
 playlist_features = pd.concat([playlist_features, top_artist_dummies], axis=1)
 playlist_features.drop(['artist_uri_top'], axis=1, inplace=True)
-playlist_features.to_csv('./data/playlist_features_with_artists.csv')
-
-# Set desired number of clusters
-n_clusters = int(np.sqrt(len(playlist_features)))
-
-print('Making clusters')
-# Make clusters
-kmeans = KMeans(n_clusters=n_clusters, verbose=0, algorithm='auto')
-kmeans.fit(playlist_features)
-
-
-print('Saving clusters')
-# Saving the clusters
-pickle.dump(kmeans, open('./model/kmeans_cluster_train.pickle', 'wb'))
-cluster_centers = kmeans.cluster_centers_
-np.savetxt('./model/kmeans_cluster_centers_train.csv', cluster_centers, delimiter=',')
-
-# Saving the cluster label for each playlist in train (e.g., for track frequency table by cluster)
-cluster_labels = kmeans.labels_
-playlist_cluster_labels = np.column_stack((playlist_list, cluster_labels))
-np.savetxt('./model/playlist_cluster_labels_train.csv', playlist_cluster_labels, delimiter=',')
+playlist_features.to_csv('./data/playlist_features_with_artists_train.csv')
