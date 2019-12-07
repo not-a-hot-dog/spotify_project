fa16db2c0a90746c65cbdcf4323632606f9510df
mattfinney <MattFinney@users.noreply.github.com>
2019-12-06 21:53:49 -0500

    Updating helper functions so that they don't read data from the disk with every call.

--- model/helper_functions.py
+++ model/helper_functions.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import numpy as np
 
+
 # Helper function to get tracks for a pid
-def get_tracks(pid, playlistfile = './data/playlists.csv'):
-    playlist_df = pd.read_csv(playlistfile)
+def get_tracks(pid, playlist_df):
     tracks = list(playlist_df.loc[playlist_df['pid'] == pid, 'track_uri'])
     return tracks
 
+
 # Helper function to get summary of track features from an array of tracks (e.g., get_tracks output)
-def get_summary_features(track_uri_array, trackfile = './data/songs_100000_feat_cleaned.csv'):
-    track_df = pd.read_csv(trackfile, index_col='track_uri')
-    track_df = track_df.loc[track_uri_array,:]
-    features_mean = track_df.describe().loc[['std'], :].reset_index(drop = True)
+def get_summary_features(track_uri_array, track_df):
+    subset_track_df = track_df.loc[track_uri_array, :]
+    features_mean = subset_track_df.describe().loc[['std'], :].reset_index(drop=True)
     features_mean.columns = [str(col) + '_mean' for col in features_mean.columns]
-    features_std = track_df.describe().loc[['std'], :].reset_index(drop = True)
+    features_std = subset_track_df.describe().loc[['std'], :].reset_index(drop=True)
     features_std.columns = [str(col) + '_std' for col in features_std.columns]
-    artist_uri_freq = track_df.artist_uri.value_counts(normalize=True)[0]
+    artist_uri_freq = subset_track_df.artist_uri.value_counts(normalize=True)[0]
     if artist_uri_freq > 0.3:  # If the top artist doesn't have 30% of track in the playlist, ignore
-        top_artist = pd.DataFrame([{'artist_uri_top': track_df.artist_uri.value_counts(normalize=True).index[0],
+        top_artist = pd.DataFrame([{'artist_uri_top': subset_track_df.artist_uri.value_counts(normalize=True).index[0],
                                     'artist_uri_freq': artist_uri_freq
                                     }]).reset_index(drop=True)
     else:
@@ -26,9 +26,10 @@ def get_summary_features(track_uri_array, trackfile = './data/songs_100000_feat_
     features = pd.concat([features_mean, features_std, top_artist], axis=1, sort=False)
     return features
 
-def build_playlist_features(pid_list):
+
+def build_playlist_features(pid_list, track_df, playlist_df):
     output = pd.DataFrame()
     for pid in pid_list:
-        output = output.append(get_summary_features(get_tracks(pid)))
+        output = output.append(get_summary_features(get_tracks(pid, playlist_df),track_df))
     output = output.set_index(pd.Index(pid_list))
     return output
--- model/k1_clustering_pre-processing.py
+++ model/k1_clustering_pre-processing.py
@@ -2,17 +2,25 @@ import pandas as pd
 import numpy as np
 from model.helper_functions import build_playlist_features
 
+print('Reading data into memory')
+playlist_list = np.genfromtxt('../data/train_pids.csv', skip_header=1, dtype=int)
+
+playlistfile = '../data/playlists.csv'
+playlist_df = pd.read_csv(playlistfile)
+
+trackfile = '../data/songs_100000_feat_cleaned.csv'
+track_df = pd.read_csv(trackfile, index_col='track_uri')
+
 print('Finding playlist features')
-playlist_list = np.genfromtxt('./data/train_pids.csv', skip_header=1, dtype=int)
-playlist_features = build_playlist_features(playlist_list)
-playlist_features.to_csv('./data/playlist_features_train.csv')
+playlist_features = build_playlist_features(playlist_list, )
+playlist_features.to_csv('../data/playlist_features_train.csv')
 
 print('Finding top artists')
 # Find the top artists who dominate playlists
 top_playlist_defining_artists = playlist_features.artist_uri_top.value_counts(normalize=False)
-top_playlist_defining_artists.to_csv('./data/top_playlist_defining_artists_train_all.csv', header=True)
+top_playlist_defining_artists.to_csv('../data/top_playlist_defining_artists_train_all.csv', header=True)
 top_playlist_defining_artists = playlist_features.artist_uri_top.value_counts().index.values[:50]
-np.savetxt('./data/top_playlist_defining_artists_train.csv', top_playlist_defining_artists, delimiter=',', fmt="%s")
+np.savetxt('../data/top_playlist_defining_artists_train.csv', top_playlist_defining_artists, delimiter=',', fmt="%s")
 
 # Keep only those artists who dominate playlists and one hot encode
 artists_to_keep = playlist_features.artist_uri_top.isin(top_playlist_defining_artists)
@@ -23,4 +31,4 @@ playlist_features.artist_uri_freq.fillna(0, inplace=True)
 top_artist_dummies = pd.get_dummies(playlist_features.artist_uri_top)
 playlist_features = pd.concat([playlist_features, top_artist_dummies], axis=1)
 playlist_features.drop(['artist_uri_top'], axis=1, inplace=True)
-playlist_features.to_csv('./data/playlist_features_with_artists_train.csv')
+playlist_features.to_csv('../data/playlist_features_with_artists_train.csv')
